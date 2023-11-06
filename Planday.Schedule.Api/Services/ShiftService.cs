8ffc419242b6b59d41bea61cd2ff48cfb11ef2e1
Angelina <angelinagalkina93@gmail.com>
2023-11-06 19:01:14 +0300

    Added logic to AddShift

--- Planday.Schedule.Api/Services/ShiftService.cs
+++ Planday.Schedule.Api/Services/ShiftService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Dapper;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Data.Sqlite;
 using Planday.Schedule.Api;
 using Planday.Schedule.Api.Dto;
@@ -19,7 +20,9 @@ namespace Planday.Schedule.Api.Services
 
         private const string SqlSelect = @"SELECT Id, EmployeeId, Start, End FROM Shift;";
 
-        private const string SqlInsertStart = @"INSERT INTO Shift (Id, EmployeeId, Start, End)";
+        private const string SqlInsertStartWithEmployeeId = @"INSERT INTO Shift (EmployeeId, Start, End)";
+        
+        private const string SqlInsertStartNoEmployeeId = @"INSERT INTO Shift (Start, End)";
 
         public ShiftService(IMapper mapper, IHttpContextAccessor httpContextAccessor, IConnectionStringProvider connectionStringProvider)
         {
@@ -49,25 +52,68 @@ namespace Planday.Schedule.Api.Services
             await using var sqlConnection = new SqliteConnection(_connectionStringProvider.GetConnectionString());
 
             var serviceResponse = new ServiceResponse<List<GetShiftDto>>();
+
             var shift = Mapper.Map<Shift>(newShift);
+            var sqlInsertEnd = string.Empty;
+            var sqlText = string.Empty;
+            var result = shift.Start.CompareTo(shift.End);
 
-            var sqlInsertEnd = $"VALUES({shift.Id}, {shift.EmployeeId}, '{shift.Start}', '{shift.End}')";
-            var sqlText = string.Concat(SqlInsertStart, sqlInsertEnd);
+            if (result <= 0)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = "Start time is less or equal than end time";
 
-            await sqlConnection.OpenAsync();
+                return serviceResponse;
+            }
 
-            var command = sqlConnection.CreateCommand();
-            command.CommandText = sqlText;
+            if (shift.Start.Date != shift.End.Date)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = "Start time and end time are not at the same day";
 
-            int rowsAffected = await command.ExecuteNonQueryAsync();
+                return serviceResponse;
+            }
 
-            var shifts = await sqlConnection.QueryAsync<GetShiftDto>(SqlSelect);
 
-            var shiftsDto = shifts.Select(x =>
-                new Shift(x.Id, x.EmployeeId, DateTime.Parse(x.Start), DateTime.Parse(x.End)));
+            try
+            {
+                await sqlConnection.OpenAsync();
+                using var transaction = sqlConnection.BeginTransaction(); // Begin the transaction
 
-            serviceResponse.Data = shiftsDto.Select(c => Mapper.Map<GetShiftDto>(c)).ToList();
-            
+                if (shift.EmployeeId == null)
+                {
+                    sqlInsertEnd = $"VALUES('{shift.Start}', '{shift.End}')";
+                    sqlText = string.Concat(SqlInsertStartNoEmployeeId, sqlInsertEnd);
+                }
+                else
+                {
+                    sqlInsertEnd = $"VALUES({shift.EmployeeId}, '{shift.Start}', '{shift.End}')";
+                    sqlText = string.Concat(SqlInsertStartWithEmployeeId, sqlInsertEnd);
+                }
+
+                var command = sqlConnection.CreateCommand();
+                command.CommandText = sqlText;
+                command.Transaction = transaction; // Assign the transaction to the command
+
+                int rowsAffected = await command.ExecuteNonQueryAsync();
+
+                // Commit the transaction
+                transaction.Commit();
+
+
+                var shifts = await sqlConnection.QueryAsync<GetShiftDto>(SqlSelect);
+
+                var shiftsDto = shifts.Select(x =>
+                    new Shift(x.Id, x.EmployeeId, DateTime.Parse(x.Start), DateTime.Parse(x.End)));
+
+                serviceResponse.Data = shiftsDto.Select(c => Mapper.Map<GetShiftDto>(c)).ToList();
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
+
             return serviceResponse;
         }
     }
