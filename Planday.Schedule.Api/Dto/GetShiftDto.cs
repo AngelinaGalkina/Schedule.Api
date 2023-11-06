597441b7a94a16a112411770c1971e4aa91d6cfb
Angelina <angelinagalkina93@gmail.com>
2023-11-06 22:03:45 +0300

    GetShiftDto changed format of Start and End

--- Planday.Schedule.Api/Dto/GetShiftDto.cs
+++ Planday.Schedule.Api/Dto/GetShiftDto.cs
@@ -2,13 +2,9 @@
 {
     public class GetShiftDto
     {
-        ////public int EmployeeId { get; set; }
-        ////public DateTime Start { get; set; }
-        ////public DateTime End { get; set; }
-        ///
         public long Id { get; set; }
         public long? EmployeeId { get; set; }
-        public string Start { get; set; }
-        public string End { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
     }
 }
