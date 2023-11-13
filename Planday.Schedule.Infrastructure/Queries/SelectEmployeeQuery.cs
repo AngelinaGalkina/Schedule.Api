459a33f3c81b38016e68c4e7aaf1f3576ad1cd96
Angelina <angelinagalkina93@gmail.com>
2023-11-13 21:26:11 +0300

    Infrastructure: added SelectEmployeeQuery with GetEmployeeById

---
+++ Planday.Schedule.Infrastructure/Queries/SelectEmployeeQuery.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using Planday.Schedule.Infrastructure.Providers.Interfaces;
+using Planday.Schedule.Queries;
+
+namespace Planday.Schedule.Infrastructure.Queries
+{
+    public class SelectEmployeeQuery : ISelectEmployeeQuery
+    {
+        private readonly IConnectionStringProvider _connectionStringProvider;
+
+        private const string Sql = @"SELECT Id, Name FROM Employee;";
+
+        public SelectEmployeeQuery(IConnectionStringProvider connectionStringProvider)
+        {
+            _connectionStringProvider = connectionStringProvider;
+        }
+    
+        public async Task<Employee> GetEmployeeById(long? id)
+        {
+            await using var sqlConnection = new SqliteConnection(_connectionStringProvider.GetConnectionString());
+
+            var sqlResponse = await sqlConnection.QueryAsync<EmployeeDto>(Sql);
+
+            var shifts = sqlResponse.Select(x =>
+              new Employee(x.Id, x.Name));
+
+            var employee = shifts.FirstOrDefault(x => x.Id == id);
+
+            return employee;
+        }
+    }
+}
+
---
+++ Planday.Schedule/Queries/ISelectEmployeeQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Planday.Schedule.Queries
+{
+    public interface ISelectEmployeeQuery
+    {
+        Task<Employee> GetEmployeeById(long? id);
+    }    
+}
+
