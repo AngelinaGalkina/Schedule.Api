1bd0c3c8273627073925bdd4d82b6da9ce9a260f
Angelina <angelinagalkina93@gmail.com>
2023-11-19 18:00:44 +0300

    Infrastructure: ISelectShiftsQuery - added GetEmployeeByShiftId, OverlappingShifts

--- Planday.Schedule/Queries/ISelectShiftsQuery.cs
+++ Planday.Schedule/Queries/ISelectShiftsQuery.cs
@@ -7,6 +7,8 @@ namespace Planday.Schedule.Queries
     {
         Task<IReadOnlyCollection<Shift>> AllShifts();
         Task<Shift> GetShiftById(long? id);
+        Task<IReadOnlyCollection<long?>> GetEmployeeByShiftId(long? id);
+        Task<IReadOnlyCollection<Shift>> OverlappingShifts(long? employeeId, DateTime newStart, DateTime newEnd);
     }    
 }
 
