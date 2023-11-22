ff458b4a9671165f40f8a457944fa9c22d78291b
Angelina <angelinagalkina93@gmail.com>
2023-11-22 21:47:51 +0300

    ServiceResponse - added EmployeeEmail

--- Planday.Schedule.Api/Models/ServiceResponse.cs
+++ Planday.Schedule.Api/Models/ServiceResponse.cs
@@ -4,6 +4,7 @@
     {
         public bool Success { get; set; } = true;
         public string Message { get; set; } = string.Empty;
+        public string EmployeeEmail { get; set; } = string.Empty;
         public T? Data { get; set; }
     }
 }
