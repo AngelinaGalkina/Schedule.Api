d9fb901e5e9b258698c70ccc1c6d514967ecc64c
Angelina <angelinagalkina93@gmail.com>
2023-11-25 13:48:09 +0300

    Infrastructure now has InsertShiftsQuery

---
+++ Planday.Schedule.Infrastructure/Queries/InsertShiftsQuery.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using Planday.Schedule.Infrastructure.Providers.Interfaces;
+using Planday.Schedule.Queries.Insert;
+
+namespace Planday.Schedule.Infrastructure.Queries
+{
+    public class InsertShiftsQuery : IInsertShiftsQuery
+    {
+        private readonly IConnectionStringProvider _connectionStringProvider;
+
+        public InsertShiftsQuery(IConnectionStringProvider connectionStringProvider)
+        {
+            _connectionStringProvider = connectionStringProvider;
+        }
+
+
+        private const string SqlInsertStartWithEmployeeId = @"INSERT INTO Shift (EmployeeId, Start, End) VALUES (@EmployeeId, @Start, @End)";
+        private const string SqlInsertStartNoEmployeeId = @"INSERT INTO Shift (Start, End) VALUES (@Start, @End)";
+
+        public async Task<long?> InsertShift(AddShiftDto shift)
+        {
+            await using var sqlConnection = new SqliteConnection(_connectionStringProvider.GetConnectionString());
+
+            await sqlConnection.OpenAsync();
+            await using var transaction = sqlConnection.BeginTransaction();
+
+            var sqlText = shift.EmployeeId == null
+                ? $"{SqlInsertStartNoEmployeeId}"
+                : $"{SqlInsertStartWithEmployeeId}";
+
+            await using var command = new SqliteCommand(sqlText, sqlConnection, transaction);
+
+            if (shift.EmployeeId != null)
+            {
+                command.Parameters.AddWithValue("@EmployeeId", shift.EmployeeId);
+            }
+
+            command.Parameters.AddWithValue("@Start", shift.Start);
+            command.Parameters.AddWithValue("@End", shift.End);
+
+            await command.ExecuteNonQueryAsync();
+
+            // Execute the command and get the last inserted ID
+            command.CommandText = "SELECT last_insert_rowid();";
+
+            var newShiftId = (long?)command.ExecuteScalar();
+
+            transaction.Commit();
+
+            return newShiftId;
+        }
+    }
+}
+
+
