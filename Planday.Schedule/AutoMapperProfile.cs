93ef32270c3501276c3e2ad51170fcecd282de49
Angelina <angelinagalkina93@gmail.com>
2023-11-25 13:46:45 +0300

    Domain layer now has Dto - should be moved and automapper

---
+++ Planday.Schedule/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace Planday.Schedule
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Shift, GetShiftDto>();
+            CreateMap<GetShiftDto, Shift>();
+            CreateMap<AddShiftDto, GetShiftDto>();
+            CreateMap<GetShiftDto, AddShiftDto>();
+            CreateMap<AddShiftDto, CreateShiftDto>();
+            CreateMap<CreateShiftDto, AddShiftDto>();
+        }
+    }
+}
---
+++ Planday.Schedule/CreateShiftDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Planday.Schedule
+{
+    public class CreateShiftDto
+    {
+        public long? EmployeeId { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+    }
+}
