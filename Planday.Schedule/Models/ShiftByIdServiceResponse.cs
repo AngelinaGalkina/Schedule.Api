36c00946383fb5d24ab8faab5180bdc466a1576c
Angelina <angelinagalkina93@gmail.com>
2023-11-25 13:44:33 +0300

    Moved Models folder to the domain layer

---
+++ Planday.Schedule/Models/ShiftByIdServiceResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Planday.Schedule.Models
+{
+    public class ShiftByIdServiceResponse<T>
+    {
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = string.Empty;
+        public string EmployeeEmail { get; set; } = string.Empty;
+        public T? Data { get; set; }
+    }
+}
