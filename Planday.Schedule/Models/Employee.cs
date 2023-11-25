fbab1b297b2bcd4bc0eb2083f21510a496aca9c8
Angelina <angelinagalkina93@gmail.com>
2023-11-25 16:51:27 +0300

    Domain: now has Models folder eith Shift and Employee

---
+++ Planday.Schedule/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace Planday.Schedule.Models
+{
+    public class Employee
+    {
+        public Employee(long id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public long Id { get; }
+        public string Name { get; }
+    }
+}
+
---
+++ Planday.Schedule/Models/Shift.cs
@@ -0,0 +1,23 @@
+﻿namespace Planday.Schedule.Models
+{
+    public class Shift
+    {
+        public Shift()
+        {
+        }
+
+        public Shift(long id, long? employeeId, DateTime start, DateTime end)
+        {
+            Id = id;
+            EmployeeId = employeeId;
+            Start = start;
+            End = end;
+        }
+
+        public long Id { get; set; }
+        public long? EmployeeId { get; set; }
+        public DateTime Start { get; }
+        public DateTime End { get; }
+    }
+}
+
