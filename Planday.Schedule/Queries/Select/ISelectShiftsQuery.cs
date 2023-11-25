505e5e4d6c5bec75476a2b6010e2981849863119
Angelina <angelinagalkina93@gmail.com>
2023-11-25 18:13:38 +0300

    ISelectShiftsQuery - removed AllShifts

--- Planday.Schedule/Queries/Select/ISelectShiftsQuery.cs
+++ Planday.Schedule/Queries/Select/ISelectShiftsQuery.cs
@@ -6,7 +6,6 @@ namespace Planday.Schedule.Queries.Select
 {
     public interface ISelectShiftsQuery
     {
-        Task<IReadOnlyCollection<Shift>> AllShifts();
         Task<Shift?> ShiftById(long? id);
         Task<IReadOnlyCollection<long?>> GetEmployeeByShiftId(long? id);
         Task<IReadOnlyCollection<Shift>> OverlappingShifts(long? employeeId, DateTime newStart, DateTime newEnd);
