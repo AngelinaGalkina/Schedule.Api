8b62c1203daa2cb3aaa32f6a02894b6a2101801f
Angelina <angelinagalkina93@gmail.com>
2023-11-25 16:50:11 +0300

    Infrastructure: added implementation EmailApiHandler

---
+++ Planday.Schedule.Infrastructure/ApiClient/EmailApiHandler.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Planday.Schedule.ApiClient;
+using RestSharp;
+
+namespace Planday.Schedule.Infrastructure.ApiClient
+{
+    public class EmailApiHandler : IEmailApiHandler
+    {
+        public string EmployeeEmail(long employeeId)
+        {
+            var url = $"http://planday-employee-api-techtest.westeurope.azurecontainer.io:5000/employee/{employeeId}";
+            var client = new RestClient(url);
+            var request = new RestRequest(url, Method.Get);
+
+            request.AddHeader("accept", "*");
+            request.AddHeader("Authorization", "8e0ac353-5ef1-4128-9687-fb9eb8647288");
+
+            var response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                var responseData = response.Content;
+
+                if (responseData == null)
+                {
+                    return null;
+                }
+
+                // Deserialize the JSON string into a dynamic object
+                dynamic jsonObject = JsonConvert.DeserializeObject(responseData);
+
+                // Access the "email" property
+                var email = jsonObject.email;
+
+                return email;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
+
