e57cf1a830eba20cf5ea2be62b0a2e35cd1dd396
Angelina <angelinagalkina93@gmail.com>
2023-11-26 20:42:26 +0300

    Domain: Models - now has ShiftBase and Shift is using it

--- Planday.Schedule/Models/Shift.cs
+++ Planday.Schedule/Models/Shift.cs
@@ -1,18 +1,12 @@
 ﻿namespace Planday.Schedule.Models;
 
-public class Shift
+public class Shift : ShiftBase
 {
-    public Shift(long id, long? employeeId, DateTime start, DateTime end)
+    public Shift(long id, long? employeeId, DateTime start, DateTime end) : base(employeeId, start, end) 
     {
         Id = id;
-        EmployeeId = employeeId;
-        Start = start;
-        End = end;
     }
 
     public long Id { get; set; }
-    public long? EmployeeId { get; set; }
-    public DateTime Start { get; }
-    public DateTime End { get; }
 }
 
---
+++ Planday.Schedule/Models/ShiftBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Planday.Schedule.Models;
+
+public class ShiftBase
+{
+    public ShiftBase(long? employeeId, DateTime start, DateTime end)
+    { 
+        EmployeeId = employeeId;
+        Start = start;
+        End = end;
+    }
+
+    public long? EmployeeId { get; set; }
+    public DateTime Start { get; }
+    public DateTime End { get; }
+}
