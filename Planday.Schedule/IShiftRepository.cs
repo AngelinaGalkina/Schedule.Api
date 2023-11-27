79476fad9d7a26deece95a107074756bf0c964a8
Angelina Galkina <angg@bankingcircle.com>
2023-11-27 16:55:42 +0100

    Added IShiftRepository and ShiftRepository

---
+++ Planday.Schedule/IShiftRepository.cs
@@ -0,0 +1,18 @@
+﻿using Planday.Schedule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planday.Schedule
+{
+    public interface IShiftRepository
+    {
+        Task<long?> InsertShift(ShiftBase shift);
+        Task<Shift?> ShiftById(long id);
+        Task<IReadOnlyCollection<long?>> EmployeeByShiftId(long id);
+        Task<IReadOnlyCollection<Shift>> OverlappingShifts(long employeeId, DateTime newStart, DateTime newEnd);
+        Task<Shift?> UpdateEmployeeId(long shiftId, long newEmployeeId);
+    }
+}
