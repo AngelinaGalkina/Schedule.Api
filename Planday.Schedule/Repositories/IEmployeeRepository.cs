fa9b9e7522ba0f933b4b29dd656b37c052ec1c41
Angelina <angelinagalkina93@gmail.com>
2023-12-01 10:40:54 +0300

    Application: now has IEmployeeRepository and IShiftRepository

---
+++ Planday.Schedule/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Planday.Schedule.Domain;
+using Planday.Schedule.Models;
+
+namespace Planday.Schedule.Repositories;
+
+public interface IEmployeeRepository
+{
+    Task<Employee> EmployeeById(long? id);
+    EmployeeInfo? EmployeeEmail(long employeeId);
+}
---
+++ Planday.Schedule/Repositories/IShiftRepository.cs
@@ -0,0 +1,13 @@
+﻿using Planday.Schedule.Domain;
+using Planday.Schedule.Models;
+
+namespace Planday.Schedule.Repositories;
+
+public interface IShiftRepository
+{
+    Task<long?> InsertShift(ShiftBase shift);
+    Task<Shift?> ShiftById(long id);
+    Task<IReadOnlyCollection<long?>> EmployeeByShiftId(long id);
+    Task<IReadOnlyCollection<Shift>> OverlappingShifts(long employeeId, DateTime newStart, DateTime newEnd);
+    Task<Shift?> UpdateEmployeeId(long shiftId, long newEmployeeId);
+}
